715fce7f9bec99409cfd0f9e8693e1bb3563a689
xinhuang <xinhuang.abc@gmail.com>
2019-02-27 15:43:12 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='PyFFF',
-    version='0.1.0'
+    version='0.1.0',
     description='Python For Filesystem Forensics',
     long_description=long_description,
     long_description_content_type='text/markdown',
