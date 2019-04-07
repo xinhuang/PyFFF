8d2a2a9ab249204127d9d8a5b22ec30dc4707374
xinhuang <xinhuang.abc@gmail.com>
2019-04-07 02:01:29 -0400

    Parse non-resident attribute header; parse data runs

--- fff/ntfs/mft_attr.py
+++ fff/ntfs/mft_attr.py
@@ -25,6 +25,21 @@ TYPES: Dict[int, str] = {
 }
 
 
+def parse_data_runs(data, offset):
+    data_runs = []
+    while data[offset] != 0:
+        header = data[offset]
+        nlength = header >> 4
+        noffset = header & 0x0F
+
+        offset += 1 + nlength + noffset
+        data_runs.append((int.from_bytes(data[offset+1:offset+1+nlength], byteorder='big'),
+                          int.from_bytes(data[offset+1+nlength:offset+1+nlength+noffset], byteorder='big')))
+
+    offset += 1
+    return offset, data_runs
+
+
 class MFTAttr(object):
 
     def __init__(self, data: bytes, offset: int, filesystem):
@@ -37,31 +52,52 @@ class MFTAttr(object):
         self.flags = struct.unpack('<H', data[offset+12:offset+14])[0]
         self.attr_id = struct.unpack('<H', data[offset+14:offset+16])[0]
 
+        name_offset = offset + self.name_offset
+        self.name = data[name_offset:name_offset+self.name_length*2]
+        of = name_offset + self.name_length * 2
+        (self.attr_length, self.attr_offset, self.index_flag,
+         self.padding) = struct.unpack('<IHBB', data[of:of+8])
+
         self.raw = data[offset:offset+self.size]
 
         if self.non_resident:
-            pass
-        else:
-            name_offset = offset + self.name_offset
-            self.name = data[name_offset:name_offset+self.name_length*2]
-            of = name_offset + self.name_length * 2
-            (self.attr_length, self.attr_offset, self.index_flag,
-             self.padding) = struct.unpack('<IHBB', data[of:of+8])
+            self.starting_vcn = struct.unpack('<Q', data[offset+0x10:offset+0x18])[0]
+            self.last_vcn = struct.unpack('<Q', data[offset+0x18:offset+0x20])[0]
+            self.offset_dataruns = struct.unpack('<H', data[offset+0x20:offset+0x22])[0]
+            self.compression_unit_size = struct.unpack('<H', data[offset+0x22:offset+0x24])[0]
+            (self.padding, self.allocated_size, self.actual_size, self.compressed_size) = struct.unpack(
+                '<IQQQ', data[offset+0x24:offset+0x40])
+            _, self.data_runs = parse_data_runs(data, offset + self.offset_dataruns)
+
+    @property
+    def compressed(self) -> bool:
+        return self.compression_unit_size > 0
 
     @property
     def type_s(self) -> str:
         return TYPES.get(self.type_id, 'Unrecognized')
 
     def tabulate(self) -> List[Sequence[Any]]:
-        return [('Type ID', '{} ({})'.format(self.type_id, self.type_s)),
-                ['Name', self.name],
-                ['Size', self.size],
-                ['Non-Resident Flag', self.non_resident],
-                ['Name Length', self.name_length],
-                ['Name Offset', self.name_offset],
-                ['Flags', bin(self.flags)],
-                ['Attribute ID', self.attr_id],
-                ['Attribute Length', self.attr_length], ]
+        r: List[Sequence[Any]] = [('Type ID', '{} ({})'.format(self.type_id, self.type_s)),
+                                  ['Name', self.name],
+                                  ['Size', self.size],
+                                  ['Non-Resident Flag', '{} ({})'.format(self.non_resident,
+                                                                         'Non-Resident' if self.non_resident else 'Resident')],
+                                  ['Name Length', self.name_length],
+                                  ['Name Offset', self.name_offset],
+                                  ['Flags', bin(self.flags)],
+                                  ['Attribute ID', self.attr_id],
+                                  ['Attribute Length', self.attr_length], ]
+        if self.non_resident:
+            r += [('Starting VCN', self.starting_vcn),
+                  ('Last VCN', self.last_vcn),
+                  ('DataRuns Offset', self.offset_dataruns),
+                  ('Compression Unit Size', self.compression_unit_size),
+                  ('Allocated Size', self.allocated_size),
+                  ('Actual Size', self.actual_size),
+                  ('Compressed Size', self.compressed_size),
+                  ('#Data Runs', len(self.data_runs)), ]
+        return r
 
     def __str__(self):
         return tabulate(self.tabulate(),
